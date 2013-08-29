27bffb577a0d3fbd44ab08561fa0fbd98f64ed9f
GeorgeXing <im.hroll@gmail.com>
2013-08-28 20:47:04 -0700

    more map types

--- playground/custom_map.js
+++ playground/custom_map.js
@@ -91,34 +91,51 @@ exports.randomEdgeCase = randomEdgeCase;
 
 function randomTriage() {
   var k = 10;
-  var m = 3*k + 2, n = 13;
+  var m = 3*k + 4, n = 13;
   var map = new DenseMap(m, n, 0, 'Triage');
 
   //Three regions, k wide and n tall.
   //Separate the three regions.
   var allJs = range(0, n);
-  map.set(tiles.EMPTY, [k, 2*k+1], allJs);
-  //Put IN1 and IN2 in between the regions.
-  map.set(tiles.TELE_IN_1, k, n/2 >> 0);
-  map.set(tiles.TELE_IN_2, 2*k+1, n/2 >> 0);
-  //First region: Start, A, B
-  //Second region: Out1, C, D
-  //Third region: Out2, E, End
-  var I1 = range(0, k);
-  var I2 = range(k+1, 2*k+1);
-  var I3 = range(2*k+2, 3*k+2);
+  map.set(tiles.EMPTY, [0,k+1, 2*k+2, 3*k+3], allJs);
+  //permute the ins/outs
+  var p = [3,4,0,1,2];
+  var INS = [], OUTS = [];
+  for (var i = 0; i < 5; i++) {
+    INS.push(tiles.TELE_INS[p[i]]);
+    OUTS.push(tiles.TELE_OUTS[p[i]]);
+  }
 
-  map.placeRandomlyInArea(tiles.GREEN_START, I1, allJs);
+  // //Put IN1 and IN2 in between the regions.
+  // map.set(INS[0], k, n/2 >> 0);
+  map.set(' ', k+1, n/2 >> 0);
+  // map.set(INS[1], 2*k+1, n/2 >> 0);
+  map.set(' ', 2*k+2, n/2 >> 0);
+  //First region: Start, A,
+  //Second region: Out1, B
+  //Third region: Out2, C, [End]
+  //Each region has a warp
+  var I1 = range(1, k+1);
+  var I2 = range(k+2, 2*k+2);
+  var I3 = range(2*k+3, 3*k+3);
+
+  map.set(tiles.GREEN_START, 0, n/2 >> 0);
+  //map.placeRandomlyInArea(tiles.GREEN_START, I1, allJs);
   map.placeRandomlyInArea(tiles.CHECKPOINT_1, I1, allJs);
-  map.placeRandomlyInArea(tiles.CHECKPOINT_2, I1, allJs);
+  map.placeRandomlyInArea(INS[2], I1, allJs);
+  map.placeRandomlyInArea(OUTS[2], I1, allJs);
 
-  map.placeRandomlyInArea(tiles.TELE_OUT_1, I2, allJs);
-  map.placeRandomlyInArea(tiles.CHECKPOINT_3, I2, allJs);
-  map.placeRandomlyInArea(tiles.CHECKPOINT_4, I2, allJs);
 
-  map.placeRandomlyInArea(tiles.TELE_OUT_2, I3, allJs);
-  map.placeRandomlyInArea(tiles.CHECKPOINT_5, I3, allJs);
-  map.placeRandomlyInArea(tiles.FINISH, I3, allJs);
+  //map.placeRandomlyInArea(OUTS[0], I2, allJs);
+  map.placeRandomlyInArea(tiles.CHECKPOINT_2, I2, allJs);
+  map.placeRandomlyInArea(INS[3], I2, allJs);
+  map.placeRandomlyInArea(OUTS[3], I2, allJs);
+
+  map.set(tiles.FINISH, m-1, n/2 >> 0);
+  //map.placeRandomlyInArea(OUTS[1], I3, allJs);
+  map.placeRandomlyInArea(tiles.CHECKPOINT_3, I3, allJs);
+  map.placeRandomlyInArea(INS[4], I3, allJs);
+  map.placeRandomlyInArea(OUTS[4], I3, allJs);
 
   //Tune some parameters
   map.walls = 21;
@@ -127,9 +144,8 @@ function randomTriage() {
 
   return map;
 }
-
 function randomYOLT() {
-  var m = 21, n = 11;
+  var m = 25, n = 11;
   var map = new DenseMap(m, n, 0, 'You Only Live Twice');
   var allJs = range(0, n);
 
@@ -166,14 +182,14 @@ function randomYOLT() {
 
   //Tune some parameters
   map.walls = 21;
-  var numExtraRocks = getRandomInt(17,23);
+  var numExtraRocks = 2+getRandomInt(17,23);
   map.placeRandomly(tiles.ROCK, numExtraRocks);
 
   return map;
 }
 
 function randomNoEnd() {
-    var m = 21, n = 11;
+  var m = 25, n = 11;
   var map = new DenseMap(m, n, 0, 'No End in Sight');
   var allJs = range(0, n);
 
@@ -210,26 +226,68 @@ function randomNoEnd() {
 
   //Tune some parameters
   map.walls = 21;
-  var numExtraRocks = getRandomInt(17,23);
+  var numExtraRocks = 3+getRandomInt(17,23);
   map.placeRandomly(tiles.ROCK, numExtraRocks);
 
   return map;
 }
 
+function randomDoubleNormal() {
+  var k = 9;
+  var m = 15, n = 2*k+1;
+  var map = new DenseMap(m, n, 0, 'Double Normal');
+  var allIs = range(0, m);
+  var J1 = range(0,k);
+  var J2 = range(k+1, 2*k+1);
+
+  //middle row is empty
+  map.set(tiles.EMPTY, allIs, k);
+  //put top start and finish
+  map.set(tiles.GREEN_START, 0, J1);
+  map.set(tiles.FINISH, m-1, J1);
+  //put bot start and finish
+  map.set(tiles.RED_START, m-1, J2);
+  map.set(tiles.FINISH, 0, J2);
+
+  //Tune some parameters
+  map.walls = getRandomInt(17, 19);
+  var numCheckpoints;
+  if (Math.random() < .75) {
+    numCheckpoints = 2;
+  } else {
+    numCheckpoints = 3;
+  }
+  for (var i = 0; i < numCheckpoints; i++) {
+    map.placeRandomlyInArea(tiles.CHECKPOINTS[i], allIs, J1);
+    map.placeRandomlyInArea(tiles.CHECKPOINTS[i], allIs, J2);
+  }
+
+  var numExtraRocks = getRandomInt(11,15);
+  map.placeRandomlyInArea(tiles.ROCK, allIs, J1, numExtraRocks);
+  map.placeRandomlyInArea(tiles.ROCK, allIs, J2, numExtraRocks);
+
+  return map;
+}
+
+function addMap(map_arr, map) {
+  var mapStr = map.myName + ':\n';
+  mapStr += map.toMapCode();
+  map_arr.push(mapStr);
+}
 
 var main = function() {
   var fs = require('fs');
+
+  var maps = [];
+  addMap(maps, randomLayover());
+  addMap(maps, randomBombsAway());
+  addMap(maps, randomEdgeCase());
+  addMap(maps, randomTriage());
+  addMap(maps, randomYOLT());
+  addMap(maps, randomNoEnd());
+  addMap(maps, randomDoubleNormal());
   
-  // var map = randomLayover();
-  // var map = randomBombsAway();
-  // var map = randomEdgeCase();
-  // var map = randomTriage();
-  // var map = randomYOLT();
-  var map = randomNoEnd();
-  var outStr = map.repr();
-  outStr += '\n\n';
-  outStr += map.toMapCode();
-  
+  var outStr = maps.join('\n\n');
   // var code = "13x7.c1.r10.w9.t0.Simple.:0s.0r.10f.0s.5r.5f.0s.0r.6r.1r.1f.0s.11f.0s.2r.4a.3f.0s.1r.4r.4f.0s.2r.6r.1f.";
 
   // var map = parseMapCode(code);
