a259467e8616fdce07dacfba2874e1a8e907e00f
GeorgeXing <im.hroll@gmail.com>
2013-11-26 15:25:13 -0500

    pathing on ugli-pathery

--- ugli-pathery/js/mapspecs.js
+++ ugli-pathery/js/mapspecs.js
@@ -107,19 +107,83 @@ function getmapdata(mapid) {
 function doSend(mapid) {
   if (solution[mapid] == undefined) { getmapdata(mapid); }
 
+  var board = mapdata[mapid];
   var mapcode = mapdata[mapid].code;
   var sol = solution[mapid];
 
-   console.log(mapcode)
-   console.log(sol)
+  console.log(mapcode)
+  console.log(sol)
 
-  $.ajax({
-    type: 'POST',
-    mapcode: mapcode,
-    solution: sol,
-    success: request_path_done
-  });
+  // $.ajax({
+  //   type: 'POST',
+  //   mapcode: mapcode,
+  //   solution: sol,
+  //   success: request_path_done
+  // });
+  
+  // TODO: async
+  request_path_done(getPath(board, sol));
 
+}
+
+function getPath(boardObj, sol) {
+  var mapcode = boardObj.code;
+
+  // var moves = 6;
+  // var path = [];
+  // path[0] = {
+  //   blocked: false,
+  //   start: "0,1",
+  //   end: "6,1",
+  //   moves: moves,
+  //   path: "f222222r",
+  // };
+
+  // return {
+  //   blocked: false,
+  //   isAChallenge: false,
+  //   mapCodeExecuted: mapcode,
+  //   mapid: 0,
+  //   totalMoves: moves,
+  //   path: path,
+  // };
+
+  // var mapgraph = new PatheryGraph(boardObj);
+  // parse blocks
+  var solTokens = sol.split('.');
+  var blocks = [];
+  // ignore first and last element
+  for (var i = 1; i < solTokens.length - 1; i++) {
+    var blockStr = solTokens[i];
+    var blockTokens = blockStr.split(',');
+
+    var a = parseInt(blockTokens[0]);
+    var b = parseInt(blockTokens[1]);
+    blocks.push([a-1,b]);
+  }
+
+  var pathInfo = compute_solution(parse_board(mapcode), blocks);
+  var totalMoves = pathInfo.value;
+  var path = pathInfo.path;
+  var blocked = false;
+  for (var i = 0; i < path.length; i++) {
+    if (path[i].blocked) {
+      blocked = true;
+      break;
+    }
+  }
+  if (blocked) {
+    totalMoves = 0;
+  }
+
+  return {
+    blocked: blocked,
+    isAChallenge: false,
+    mapCodeExecuted: mapcode,
+    mapid: 0,
+    totalMoves: totalMoves,
+    path: path,
+  };
 }
 
 function request_path_done(JO) {
@@ -203,12 +267,14 @@ function animatePath(path, mapid, start, pathNumber) {
 	var c = p.substring(0, 1);
 	p = p.substring(1);
 
-	document.getElementById(mapid+',btn').disabled = true;
+  // TODO: fix
+	// document.getElementById(mapid+',btn').disabled = true;
 	doanimate(x, y, p, c, mapid, pathNumber);
 }
 
 function animatePathDone(mapid) {
-	document.getElementById(mapid+',btn').disabled = false;
+  // TODO: fix
+	// document.getElementById(mapid+',btn').disabled = false;
 }
 
 function doanimate(x, y, p, c, mapid, pathNumber) {
@@ -713,4 +779,475 @@ function getCookie(c_name)
 		}
 	}
 	return "";
+}
+
+
+// PATHING
+// TODO: require.js?
+
+ROCK_1             = 'r';
+ROCK_2             = 'R';  // never used?
+ROCK_3             = 'q';  // used in seeing double, same as rock?
+
+PATCH              = 'p';  // can't place blocks, but path can pass
+
+GREEN_THROUGH_ONLY = 'X';  // colored green, blocks red
+RED_THROUGH_ONLY   = 'x';  // colored red, blocks green
+
+GREEN_START        = 's';
+RED_START          = 'S';
+
+FINISH             = 'f';
+
+CHECKPOINT_1       = 'a';
+CHECKPOINT_2       = 'b';
+CHECKPOINT_3       = 'c';
+CHECKPOINT_4       = 'd';
+CHECKPOINT_5       = 'e';
+CHECKPOINTS        = [CHECKPOINT_1, CHECKPOINT_2, CHECKPOINT_3, CHECKPOINT_4, CHECKPOINT_5];
+// dark blue
+TELE_IN_1          = 't';
+TELE_OUT_1         = 'u';
+// green
+TELE_IN_2          = 'm';
+TELE_OUT_2         = 'n';
+// red
+TELE_IN_3          = 'g';
+TELE_OUT_3         = 'h';
+// light blue
+TELE_IN_4          = 'i';
+TELE_OUT_4         = 'j';
+// light green
+TELE_IN_5          = 'k';
+TELE_OUT_5         = 'l';
+
+teleports_map = {};
+teleports_map[TELE_IN_1] = TELE_OUT_1;
+teleports_map[TELE_IN_2] = TELE_OUT_2;
+teleports_map[TELE_IN_3] = TELE_OUT_3;
+teleports_map[TELE_IN_4] = TELE_OUT_4;
+teleports_map[TELE_IN_5] = TELE_OUT_5;
+
+PATH_BLOCKED_CONSTANT = NaN; // TODO: use this
+
+function PatheryGraph(board) {
+
+  this.board = board; // i,j -> val
+  this.n = board.length;
+  this.m = board[0].length;
+
+  this.serial_board = []; // same as board, but uses keyified index
+  this.boardstuff = {}; // reverse of serial board.  val -> list of keyified blocks
+
+  // Note that these lists start from top-left and go right, then down
+  // In particular, starts and finishes are ordered top to bottom
+  // Also when there are multiple outs for teleports, same ordering is used
+  for (var i = 0; i < this.n; i++) {
+    for (var j = 0; j < this.m; j++) {
+      var stuff = this.board[i][j];
+      var key = this.keyify_coordinates(i,j);
+      this.serial_board.push(stuff);
+      if (stuff != ' ') {
+        if (this.boardstuff.hasOwnProperty(stuff)) {
+          this.boardstuff[stuff].push(key);
+        } else {
+          this.boardstuff[stuff] = [key]
+        }
+      }
+    }
+  }
+
+  this.green_starts = this.boardstuff[GREEN_START]; // list of keyified starts
+  this.has_regular = (this.green_starts !== undefined);
+
+  this.red_starts = this.boardstuff[RED_START]; // list of keyified alt-starts
+  this.has_reverse = (this.red_starts !== undefined);
+
+  this.checkpoints = []; // list of lists of intermediate targets, including starts and ends, all keyified
+
+  var checkpoint_types = [CHECKPOINT_1, CHECKPOINT_2, CHECKPOINT_3, CHECKPOINT_4, CHECKPOINT_5];
+  for (var i = 0; i < 5; i++) {
+    var checkpoint_type = checkpoint_types[i];
+    if (!(this.boardstuff.hasOwnProperty(checkpoint_type))) {
+      break;
+    }
+    this.checkpoints.push(this.boardstuff[checkpoint_type]);
+  }
+
+  this.finishes = this.boardstuff[FINISH]; // list of keyified finishes
+
+  this.teleports = {};
+  for (teleport_key in teleports_map) {
+    // TODO: NOT TRUE IN GENERAL!!!
+    var teleport_ins = this.boardstuff[teleport_key];
+    if (! teleport_ins) {continue;}
+    var teleport_outs = this.boardstuff[teleports_map[teleport_key]];
+
+    for (var i = 0; i < teleport_ins.length; i++) {
+      this.teleports[teleport_ins[i]] = teleport_outs;
+    }
+  }
+
+  // NOTE: Order is important.  DETERMINES MOVE PRIORITIES
+  this.moves = [[-1, 0], [0, 1], [1, 0], [0, -1]];
+
+  // Preprocess neighbors
+  // Index is the keyified block
+  this.neighbors = [];
+  var neighbors_list;
+  var xp;
+  var yp;
+  for (var x = 0; x < this.n; x++) {
+    for (var y = 0; y < this.m; y++) {
+      neighbors_list = [];
+      for (var i = 0; i < this.moves.length; i++) {
+        xp = x + this.moves[i][0];
+        yp = y + this.moves[i][1];
+
+        // fill edge with rocks so we don't need this check?
+        if (((0 <= xp) && (xp < this.n)) && ((0 <= yp) && (yp < this.m))) {
+          val = this.board[xp][yp];
+          if (val == ROCK_1) {continue;}
+          if (val == ROCK_2) {continue;}
+          if (val == ROCK_3) {continue;}
+          neighbors_list.push(this.keyify_coordinates(xp, yp));
+        }
+      }
+      this.neighbors.push(neighbors_list);
+    }
+  }
+
+}
+
+PatheryGraph.prototype.keyify_coordinates = function(x, y) {
+  return x * this.m + y;
+}
+
+PatheryGraph.prototype.keyify = function(block) {
+  return this.keyify_coordinates(block[0] , block[1]);
+}
+
+PatheryGraph.prototype.unkeyify = function(blockkey) {
+  return [Math.floor(blockkey / this.m), blockkey % this.m];
+}
+
+PatheryGraph.prototype.snapify = function(keyed) {
+  var unkeyed = this.unkeyify(keyed);
+  return [unkeyed[1], unkeyed[0] + 1];
+}
+
+PatheryGraph.prototype.path_dir = function(oldkey, newkey) {
+  var diff = newkey - oldkey;
+  switch(diff) {
+    case -this.m:
+      return '1'; // up
+    case 1:
+      return '2'; // right
+    case this.m:
+      return '3'; // down
+    case -1:
+      return '4'; // left
+    default:
+      throw new Error("unexpected value in pathing");
+  }
+}
+
+PatheryGraph.prototype.dictify_blocks = function(blocks_list) {
+  var blocks_dict = {};
+  for (var i =0; i < blocks_list.length; i++) {
+    blocks_dict[this.keyify(blocks_list[i])] = true;
+  }
+  return blocks_dict;
+}
+
+PatheryGraph.prototype.listify_blocks = function(blocks_dict) {
+  var blocks_list = [];
+  for (var block in blocks_dict) {
+    blocks_list.push(this.unkeyify(block));
+  }
+  return blocks_list;
+}
+
+PatheryGraph.prototype.teleport = function(block, used_teleports) {
+  var stuff = this.serial_board[block];
+  if ( teleports_map[stuff] ) {
+    if (!(used_teleports[stuff])) {
+      used_teleports[stuff] = true;
+      return this.teleports[block]
+    }
+  }
+  return null;
+}
+
+// var BFS_queue = new Int32Array(graph.m * graph.n); // new Array(...)
+var BFS_queue = new Int32Array(1000); // new Array(...)
+
+PatheryGraph.prototype.find_path = function(
+             blocks, // currently placed blocks
+             extra_block, // unpassable square (used for green or red only)
+             sources, // list of source vertices, in order of priority
+             targets // set of target vertices
+            ) {
+  parent_map = []; // keyified index ->  parent key (or -1 if was source, and undefined if not yet reached)
+  var queue = BFS_queue;
+  var queue_start = 0,
+      queue_end = 0;
+
+  for (var k in sources) {
+    var source = sources[k];
+    queue[queue_end++] = source;
+    parent_map[source] = -1;
+  }
+
+  while (queue_start != queue_end) {
+    var u = queue[queue_start++];
+
+    var neighbors = this.neighbors[u];
+    for (var i = 0; i < neighbors.length; i++) {
+      var v = neighbors[i];
+
+      // already found this square
+      if (parent_map.hasOwnProperty(v)) { continue;}
+
+      if (blocks[v]) {continue;}
+
+      // impassable square
+      if (this.serial_board[v] === extra_block) {continue;}
+
+      parent_map[v] = u;
+
+      // found target!
+      if (targets[v]) {
+        var path = [];
+        while (v !== -1) {
+          path.push(v);
+          v = parent_map[v];
+        }
+        return path.reverse();
+      }
+
+      // add to queue
+      queue[queue_end++] = v;
+    }
+  }
+  return null;
+}
+
+function find_full_path(graph, blocks, reversed) {
+  var used_teleports = {};
+  var fullpathinfo = [];
+  var cur; // current list of start points
+  var extra_block;
+  if (reversed) {     // red path
+    cur = graph.red_starts;
+    extra_block = GREEN_THROUGH_ONLY;
+  } else {            // green path
+    cur = graph.green_starts;
+    extra_block = RED_THROUGH_ONLY;
+  }
+  var num_teleports_used = 0;
+
+  var start_block_str, end_block_str;
+  var block, last_block;
+
+  var lastIndex = -1, index = 0;
+  var totalMoves = 0;
+  while (index < graph.checkpoints.length  + 1) {
+    var target_dict = {}
+    if (index == graph.checkpoints.length) {
+      var targets = graph.finishes;
+      var cpname = FINISH;
+    } else if (reversed)  {
+      var targets = graph.checkpoints[graph.checkpoints.length - 1 - index];
+      var cpname = CHECKPOINTS[graph.checkpoints.length - 1 - index];
+    } else {
+      var targets = graph.checkpoints[index];
+      var cpname = CHECKPOINTS[index];
+    }
+    for (var i in targets) {
+      var target = targets[i];
+      target_dict[target] = true;
+    }
+    var path = graph.find_path(blocks, extra_block, cur, target_dict); // returns a list of keys, or null
+    if (path == null) {
+      if (!start_block_str) {
+        // TODO: make sure this is right? it doesn't seem to matter
+        start_block_str = stringify_block(graph.snapify(cur[0]));
+      }
+      if (!end_block_str) {
+        // TODO: make sure this is right? it doesn't seem to matter
+        end_block_str = stringify_block(graph.snapify(graph.finishes[0]));
+      }
+      var lastTarget = cpname;
+      var totalMoves = NaN;
+      var retpath = {
+        blocked: true,
+        start: start_block_str,
+        end: end_block_str,
+        lastTarget: lastTarget,
+        path: fullpathinfo.join(''),
+      };
+      return {
+        path: retpath,
+        value: totalMoves,
+      };
+    }
+    var out_blocks = null;
+
+    if (lastIndex == index) {
+      // we hit a teleporter last time
+      console.log('whee' + stringify_block(graph.snapify(path[0])));
+      fullpathinfo.push(graph.serial_board[path[0]]);
+      fullpathinfo.push(stringify_block(graph.snapify(path[0])));
+      fullpathinfo.push(graph.serial_board[path[0]]);
+    } else {
+      if (lastIndex == -1) {
+        // first pass: initialize
+        start_block_str = stringify_block(graph.snapify(path[0]));
+      }
+      // initialize this checkpoint
+      fullpathinfo.push(cpname);
+    }
+    
+    // push things onto actual path, until we hit a teleport
+    last_block = path[0];
+    for (var k = 1; k < path.length; k++) {
+      totalMoves += 1;
+      block = path[k];
+      fullpathinfo.push(graph.path_dir(last_block, block));
+      last_block = block;
+
+      out_blocks = graph.teleport(block, used_teleports);
+      if (out_blocks != null) {
+        // fullpath.push(block);
+        num_teleports_used += 1;
+        cur = out_blocks;
+
+        var tpchar = graph.serial_board[block];
+        fullpathinfo.push(tpchar);
+        break;
+      } else if (k == path.length - 1) {
+        // last step in the path for this checkpoint
+        if (index == graph.checkpoints.length) {
+          // special case: last step in the entire path
+          end_block_str = stringify_block(graph.snapify(block));
+        }
+        fullpathinfo.push('r'); // signify done with this checkpoint
+      }
+    }
+
+    lastIndex = index;
+
+    if (out_blocks == null) {
+      index += 1;
+      cur = [block];
+    }
+  }
+  var retpath = {
+    blocked: false,
+    start: start_block_str,
+    end: end_block_str,
+    path: fullpathinfo.join(''),
+    moves: totalMoves,
+  };
+  return {
+    path: retpath,
+    value: totalMoves,
+  };
+}
+
+function find_pathery_path(graph, blocks){
+  var path = [];
+  var values = [];
+
+  if (graph.has_regular) {
+    solution_green = find_full_path(graph, blocks);
+    path.push(solution_green.path);
+    console.log(path[0].path);
+    values.push(solution_green.value);
+  }
+
+  if (graph.has_reverse) {
+    solution_red = find_full_path(graph, blocks, true);
+    path.push(solution_red.path);
+    values.push(solution_red.value);
+  }
+  return {path: path,
+          value: sum_values(values),
+  };
+}
+
+
+function compute_solution(board, cur_blocks) {
+    if (cur_blocks === undefined) {cur_blocks = []}
+    var graph = new PatheryGraph(board);
+
+    var current_blocks = graph.dictify_blocks(cur_blocks);
+    var solution = find_pathery_path(graph, current_blocks);
+
+    return solution;
+}
+
+function sum_values(array) {
+  if (array.length == 0) {return PATH_BLOCKED_CONSTANT;}
+  return array.reduce(function(x, y) {return x + y})
+}
+
+function stringify_block(block) {
+  return block[0] + ',' + block[1];
+}
+
+function parse_board(code) {
+  var head = code.split(':')[0];
+  var body = code.split(':')[1];
+
+  var head = head.split('.');
+  var dims = head[0].split('x');
+  var width = parseInt(dims[0]);
+  var height = parseInt(dims[1]);
+
+  if (head[1][0] != 'c') {console.log('head[1][0] was ' + head[1][0] + ' expected c');}
+  var targets = parseInt(head[1].slice(1));
+
+  if (head[2][0] != 'r') {console.log('head[2][0] was ' + head[2][0] + ' expected r');}
+
+  if (head[3][0] != 'w') {console.log('head[3][0] was ' + head[3][0] + ' expected w');}
+  var walls_remaining = parseInt(head[3].slice(1));
+
+  if (head[4][0] != 't') {console.log('head[4][0] was ' + head[4][0] + ' expected t');}
+  var teleports = parseInt(head[4].slice(1))
+
+  var data = new Array();
+  for (i = 0; i < height; i++) {
+      var row = new Array();
+      for (j = 0; j < width; j++) {
+          row[j] = ' ';
+      }
+      data[i] = row;
+  }
+
+  var i = -1;
+  var j = width - 1;
+  var body_split = body.split('.').slice(0, -1);
+
+  for (var k = 0; k < body_split.length; k++) {
+      var item = body_split[k];
+      for (var l = 0; l < parseInt(item.slice(0, -1)) + 1; l++) {
+          j += 1;
+          if (j >= width) {
+              j = 0;
+              i += 1;
+          }
+      }
+      var type = item[item.length - 1];
+      data[i][j] = type;
+  }
+
+  //var board = [];
+  //for (var i in data) {
+  //  board.push(data[i].join(''));
+  //}
+  //return board
+
+  return data;
 }
