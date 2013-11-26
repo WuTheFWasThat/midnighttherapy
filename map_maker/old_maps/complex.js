ed4c426dce03ae326baad0310a0b8778ddeb674e
GeorgeXing <im.hroll@gmail.com>
2013-11-26 16:35:13 -0500

    complex map type

--- map_maker/old_maps/complex.js
+++ map_maker/old_maps/complex.js
@@ -8,28 +8,32 @@ var map_repr = require('../map_repr');
 var DenseMap = map_repr.DenseMap;
 
 exports.generate = function() {
-  var m = 15, n = 9;
-  var map = new DenseMap(m, n, 0, 'Normal');
+  var m = 19, n = 9;
+  var map = new DenseMap(m, n, 0, 'Complex');
 
   var allJs = range(0, n);
   // start and finish columns
   map.set(tiles.GREEN_START, 0, allJs);
   map.set(tiles.FINISH, m-1, allJs);
 
-  // A-D, ratio 1:2:2:1
+  // D-E, ratio 1:1:1:1
   var CPDist = new util.Distribution();
-  CPDist.add(1, 1);
-  CPDist.add(2, 2);
-  CPDist.add(3, 2);
+  CPDist.add(2, 1);
+  CPDist.add(3, 1);
   CPDist.add(4, 1);
+  CPDist.add(5, 1);
   var numCps = CPDist.sample();
   map.placeCheckpoints(numCps, {no_left_right: 1});
 
-  // Rock probability 1/7?
-  map.placeRocks(1/7);
+  // uniform between 1 and 2 TPs
+  var numTps = getRandomInt(1,2);
+  map.placeTps(numTps, {no_left_right: 1});
 
-  // Walls uniform from 11-13?
-  map.walls = getRandomInt(11,13);
+  // Rock probability 1/8?
+  map.placeRocks(1/8);
+
+  // Walls uniform from 14-16?
+  map.walls = getRandomInt(14,16);
 
   return map;
 }
