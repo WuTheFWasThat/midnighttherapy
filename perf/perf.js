437a7a05f8e6bf8e60c83c1e6b61c4310117c81e
GeorgeXing <im.hroll@gmail.com>
2013-12-18 02:12:08 -0800

    performance benchmarking

---
+++ perf/perf.js
@@ -0,0 +1,36 @@
+Analyst = require('../src/analyst.js');
+repr = require('../map_maker/map_repr.js');
+fs = require('fs');
+
+var files = fs.readdirSync('./tests');
+var NUMTIMES_DEFAULT = 20;
+for (var i in files) {
+  var filename = files[i];
+  var testcase = require('./tests/' + filename);
+
+  // calculate board
+  var boardDenseMap = repr.parseMapCode(testcase.code);
+  var board = boardDenseMap.toBoard();
+  // calculate solution
+  var solution = testcase.sol;
+  // calculate numTimes
+  var numTimes = testcase.numTimes;
+  if (numTimes == undefined) {
+    numTimes = NUMTIMES_DEFAULT;
+  }
+
+  // Do the test
+  console.log("Test " + filename);
+  var t = new Date().getTime();
+  for (var i = 0; i < numTimes; i++) {
+    var result = Analyst.compute_values(board, solution);
+  }
+
+  var time_elapsed = new Date().getTime() - t;
+
+  var avgTime = time_elapsed/numTimes;
+  console.log("ms elapsed:                      " , time_elapsed);
+  console.log("average time:                    " , avgTime);
+  console.log("find_pathery_path count:         " , result.find_pathery_path_count);
+  console.log("average ms / #find_pathery_path: " , avgTime / result.find_pathery_path_count)
+}
