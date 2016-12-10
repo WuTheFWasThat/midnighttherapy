faa5916d9a9f201956e0315fc02151dcbed19830
Jeff Wu <wuthefwasthat@gmail.com>
2016-12-10 12:57:36 -0800

    lint

--- pathery-server.js
+++ pathery-server.js
@@ -16,45 +16,48 @@ app.configure(function() {
 
 var middleware = [
   function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
     next();
-  }
+  },
 ];
 
 app.post('/place_greedy', middleware, function(req, res){
-  console.log("\nPLACE GREEDY:")
+  console.log('\nPLACE GREEDY:');
   var t = new Date().getTime();
-  var result = Analyst.place_greedy(JSON.parse(req.param('board')), JSON.parse(req.param('solution')), JSON.parse(req.param('remaining')));
-  console.log("ms elapsed: " , new Date().getTime() - t)
+  var result = Analyst.place_greedy(
+    JSON.parse(req.param('board')),
+    JSON.parse(req.param('solution')),
+    JSON.parse(req.param('remaining')));
+  console.log('ms elapsed: ' , new Date().getTime() - t);
 
   res.json(result);
 });
 
 app.post('/improve_solution', middleware, function(req, res){
-  console.log("\nIMPROVE:")
+  console.log('\nIMPROVE:');
   var t = new Date().getTime();
   var result = Analyst.improve_solution(JSON.parse(req.param('board')), JSON.parse(req.param('solution')), {});
 
-  console.log("ms elapsed: " , new Date().getTime() - t)
+  console.log('ms elapsed: ' , new Date().getTime() - t);
 
-  console.log(req.param('solution'))
-  console.log(result)
+  console.log(req.param('solution'));
+  console.log(result);
 
   res.json(result);
 });
 
 app.post('/play_map', middleware, function(req, res){
-  console.log("\nPLACE GREEDY:")
+  console.log('\nPLACE GREEDY:');
   var t = new Date().getTime();
   var result = Analyst.play_map(JSON.parse(req.param('board')));
-  console.log("ms elapsed: " , new Date().getTime() - t)
+  console.log('ms elapsed: ' , new Date().getTime() - t);
 
   res.json(result);
 });
 
 app.post('/compute_value', middleware, function(req, res){
   //console.log("\nCOMPUTE VALUE:")
-  var t = new Date().getTime();
+  // var t = new Date().getTime();
   var result = Analyst.compute_value(JSON.parse(req.param('board')), JSON.parse(req.param('solution')));
   //console.log("ms elapsed: " , new Date().getTime() - t)
 
@@ -63,14 +66,14 @@ app.post('/compute_value', middleware, function(req, res){
 });
 
 app.post('/compute_values', middleware, function(req, res){
-  console.log("\nCOMPUTE VALUES:")
+  console.log('\nCOMPUTE VALUES:');
   var t = new Date().getTime();
   var result = Analyst.compute_values(JSON.parse(req.param('board')), JSON.parse(req.param('solution')));
   var time_elapsed = new Date().getTime() - t;
 
-  console.log("ms elapsed: " , time_elapsed);
-  console.log("find_pathery_path count: " + result.find_pathery_path_count);
-  console.log("ms / #find_pathery_path: " , time_elapsed / result.find_pathery_path_count)
+  console.log('ms elapsed: ' , time_elapsed);
+  console.log('find_pathery_path count: ' + result.find_pathery_path_count);
+  console.log('ms / #find_pathery_path: ' , time_elapsed / result.find_pathery_path_count);
 
   res.json(result);
 });
@@ -86,28 +89,29 @@ app.post('/generate_map', middleware, function(req, res){
   var mapcode = req.param('code');
 
   var Analyst = require('./src/analyst');
-  var util = require('./map_maker/map_util');
+  // var util = require('./map_maker/map_util');
   var repr = require('./map_maker/map_repr');
 
   var map;
   if (!mapcode) {
     // TODO: what to do about different map type folders?
 
+    var map_gen;
     try {
-      var map_gen = require('./map_maker/map_types/' + maptype);
+      map_gen = require('./map_maker/map_types/' + maptype);
     } catch (e) {
-      var map_gen = require('./map_maker/old_maps/' + maptype);
+      map_gen = require('./map_maker/old_maps/' + maptype);
     }
 
     map = map_gen.generate();
-    var value = Analyst.sum_values(Analyst.compute_value(map.toBoard()))
+    var value = Analyst.sum_values(Analyst.compute_value(map.toBoard()));
     var tries = 1;
     while (isNaN(value)) {
       map = map_gen.generate();
-      var value = Analyst.sum_values(Analyst.compute_value(map.toBoard()))
+      value = Analyst.sum_values(Analyst.compute_value(map.toBoard()));
       tries += 1;
     }
-    // console.log('value', value, 'tries', tries);
+    console.log('value', value, 'tries', tries);
   } else {
     map = repr.parseMapCode(mapcode);
   }
@@ -118,37 +122,36 @@ app.post('/generate_map', middleware, function(req, res){
   var header_stuff = map.calcHeaderContents();
 
   var result = {
-    "ID":0,
-    "tiles": tiles,
+    'ID':0,
+    'tiles': tiles,
 
-    "teleports":header_stuff.t,
-    "checkpoints":header_stuff.c,
+    'teleports':header_stuff.t,
+    'checkpoints':header_stuff.c,
 
-    "width": map.m,
-    "height": map.n,
-    "walls": map.walls,
-    "name": maptype,
-    "flags":null,
-    "dateCreated":null,
-    "dateExpires":1374724800,
-    "isBlind":false,
-    "isMultiPath":false,
-    "code":code
-  }
+    'width': map.m,
+    'height': map.n,
+    'walls': map.walls,
+    'name': maptype,
+    'flags':null,
+    'dateCreated':null,
+    'dateExpires':1374724800,
+    'isBlind':false,
+    'isMultiPath':false,
+    'code':code,
+  };
   res.json(result);
 });
 
 app.post('/get_map_types', middleware, function(req, res){
   var result = {
     custom: {},
-    original: {}
-  }
-
+    original: {},
+  };
 
   function add_types(category, directory) {
     var files = fs.readdirSync(directory);
     for (var i in files) {
-      var parts = files[i].split('.')
+      var parts = files[i].split('.');
       if ((parts.length !== 2) || (parts[parts.length - 1] !== 'js')) continue;
       var type = parts[0];
       result[category][type] = require(directory + '/' + files[i]).name;
@@ -163,7 +166,7 @@ app.post('/get_map_types', middleware, function(req, res){
 
 app.get('/', middleware, function(req, res) {
   res.redirect('/ugli-pathery');
-})
+});
 
 
 app.use(express.static(__dirname));
